a6df0088375549fd73da41e0b2ec79d7a0ba78f7
koropatva <koropatvaua@gmail.com>
2014-06-01 14:30:58 +0300

    Simple RESTfull controller

--- src/main/java/org/koropatva/sich/standalone/Application.java
+++ src/main/java/org/koropatva/sich/standalone/Application.java
@@ -1,8 +1,14 @@
 package org.koropatva.sich.standalone;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("org.koropatva")
+@EnableAutoConfiguration
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Hello WORLD.");
+		SpringApplication.run(Application.class, args);
 	}
 }
